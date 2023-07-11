7dec368a8ddeeace51af42f70f49a28627ec3aff
Hinata-Ikeda <131964407+Hinata-Ikeda@users.noreply.github.com>
2023-07-11 15:10:59 +0900

    スペルカード　スペルミス

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ GameArea.refresh();
 let IsGameRunning = false;
 
 //動かすバーカ
-const ber = new Chikichikitanuki({
+const bar = new Chikichikitanuki({
     ctx: MainCanvas,
-    img: "test.png",
+    img: "assets/japanese zeikin.png",
     size: new TanukiVector(124, 15),
-    position: new TanukiVector(GameArea.x /2, GameArea.y -100),
+    position: new TanukiVector(GameArea.x /2, GameArea.y - 100),
       update: function(){
         if(keyInput.key["a"] && this.position.x > 0 + this.size.x / 2){
             this.position.x -= 10;
